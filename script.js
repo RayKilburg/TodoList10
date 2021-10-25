210fcc268e51a49860b0ee52c14ad255973505bf
RayKilburg <rkilburg42@gmail.com>
2021-10-24 20:07:22 -0400

    local storge added to todos

--- script.js
+++ script.js
@@ -3,6 +3,14 @@ const form = document.getElementById('form')
 const input = document.getElementById('input')
 const todosUL = document.getElementById('todos')
 
+// checking for local storage, intialize todo for local storage
+const todos = JSON.parse(localStorage.getItem('todos'))
+
+// checking for todos 
+if(todos) {
+    todos.forEach(todo => addTodo(todo))
+}
+
 // Event Listener for the enter button and to add todo to list
 form.addEventListener('submit', (e) => {
     e.preventDefault()
@@ -30,15 +38,21 @@ function addTodo(todo) {
         todoEl.innerText = todoText
 
         // complete todo with left click
-        todoEl.addEventListener('click', () => todoEl.classList.toggle('completed'))
+        todoEl.addEventListener('click', () => {
+            todoEl.classList.toggle('completed')
+            // update Local Storage
+            updateLS()
+        })
 
         // delete todo with right click, contextmenu = right click
         todoEl.addEventListener('contextmenu', (e) => {
             // preventing the default drop-down menu from right click
             e.preventDefault()
 
-            //removing todo from the dom
+            // removing todo from the dom
             todoEl.remove()
+            // update Local Storage
+            updateLS()
         })
 
         // add todo to dom
@@ -46,8 +60,31 @@ function addTodo(todo) {
 
         // clear form
         input.value = ''
+
+        // function that updates local storage
+        updateLS()
     }
 
     // todo text value example in the console
     console.log(todoText)
+}
+
+// function that updates the local storage
+function updateLS() {
+    // take all list items
+    todosEl = document.querySelectorAll('li')
+
+    // make todos array
+    const todos = []
+
+    // take todos element and loop through each
+    todosEl.forEach(todoEl => {
+        todos.push({
+            text: todoEl.innerText,
+            completed: todoEl.classList.contains('completed')
+        })
+    })
+
+    // updating local storage
+    localStorage.setItem('todos', JSON.stringify(todos))
 }
